2be01f8c5f3213903b2620459afa29ec1c70e09a
Tboneteen18 <tboneteen18@gmail.com>
2015-05-28 12:21:26 -0600

    added the responses from the computer

--- magic8.rb
+++ magic8.rb
@@ -7,7 +7,15 @@ class MagicEight
     puts "Ask me your question..."
     @question = gets.chomp
   end
+
+  def comp_responses
+    response = ["yes","maybe","no certainly not","never"]
+    number = rand(0..3)
+    puts response[number]
+  end
   
 end
 
 a = MagicEight.new
+a.ask_question
+a.comp_responses

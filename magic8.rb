f0bdd3691b856d750afc2d8fd972eb66e95da486
Konrad Maurer <kradjr@gmail.com>
2015-05-28 12:27:12 -0600

    Adds play method that allows player to quit and loops the program.

--- magic8.rb
+++ magic8.rb
@@ -4,18 +4,25 @@ class MagicEight
   end
 
   def ask_question
-    puts "Ask me your question..."
-    @question = gets.chomp
+    puts "Ask me your question... (or quit, I don't care.)"
+    @question = gets.chomp.upcase
   end
 
   def comp_responses
+    goodbye_message if @question == 'QUIT'
     response = ["yes","maybe","no certainly not","never"]
     number = rand(0..3)
     puts response[number]
+  end
+
+  def play
+    begin 
+      ask_question
+      comp_responses
+    end until @question == 'QUIT'
   end
   
 end
 
 a = MagicEight.new
-a.ask_question
-a.comp_responses
+a.play
